f3b6c6be181496b1e0554615229d97aeb617d0b4
Guillaume Chauveau <ch.guillaume05@gmail.com>
2025-04-23 13:51:15 +0200

    modified

--- src/App.vue
+++ src/App.vue
@@ -50,9 +50,9 @@ const currentView = computed(() => {
   <footer class="fixed_footer">
   <div class="content">
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis ducimus nemo quo totam neque quis soluta nisi obcaecati aliquam saepe dicta adipisci blanditiis quaerat earum laboriosam accusamus nesciunt! Saepe ex maxime enim asperiores nisi. Obcaecati nostrum nobis laudantium aliquam commodi veniam magni similique ullam quis pariatur voluptatem harum id error.</p>
-    <img src="./CNRS.png"/>
-    <img src="./ISJPS.png"/>
-    <img src="./logo-jfli.png"/>
+    <img src="CNRS.png"/>
+    <img src="ISJPS.png"/>
+    <img src="logo-jfli.png"/>
   </div>
 </footer>
     
