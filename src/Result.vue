a683a41ad98b4a69a6cb8b974a5e0ec6d20e7043
Guillaume Chauveau <ch.guillaume05@gmail.com>
2025-04-30 09:57:00 +0200

    teste avec onload

--- src/Result.vue
+++ src/Result.vue
@@ -39,6 +39,12 @@ const loadCSV = async (filePath) => {
     console.error('Erreur lors du chargement du fichier CSV :', error)
   }
 }
+
+// Charger les données CSV au montage du composant
+onload(() => {
+  loadCSV('/RECO/data/characteristics.csv')
+})
+
 // Fonction pour extraire les caractéristiques d'une série
 function get_features(serie_name, features, df) {
   if (!df || df.length === 0) {
