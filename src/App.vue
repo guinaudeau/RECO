363a15545023144642f406fad78f7d6bf7a77763
Guillaume Chauveau <ch.guillaume05@gmail.com>
2025-05-21 11:55:47 +0200

    création d'un formulaire de contacte

--- src/App.vue
+++ src/App.vue
@@ -161,6 +161,29 @@ changementVus()
       <a href="https://isjps.pantheonsorbonne.fr/" target="Fenêtre définie"><img src="ISJPS.png" alt="logo du ISJPS" /></a>
       <a href="https://jfli.cnrs.fr/" target="Fenêtre définie"><img src="logo-jfli.png" alt="logo du JFLI" /></a>
     </div>
+    <form action="/ma-page-de-traitement" method="post">
+      <ul>
+        <li>
+          <label for="name">Nom&nbsp;:</label>
+          <input type="text" id="name" name="user_name" />
+        </li>
+        <li>
+          <label for="mail">E-mail&nbsp;:</label>
+          <input type="email" id="mail" name="user_email" />
+        </li>
+        <li>
+          <label for="msg">Message&nbsp;:</label>
+          <textarea id="msg" name="user_message"></textarea>
+        </li>
+        <li>
+          <button type="submit">Envoyer</button>
+        </li>
+      </ul>
+    </form>
+    <div>
+      <p>Pour toute question ou suggestion, n'hésitez pas à nous contacter.</p>
+      <p>© 2023 RECO+. Tous droits réservés.</p>
+    </div>
   </footer>
 </template>
 
