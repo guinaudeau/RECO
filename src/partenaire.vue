7935b310fd9069f53a1313360f1903838674d4a5
Guillaume Chauveau <ch.guillaume05@gmail.com>
2025-06-23 09:34:08 +0200

    changement de coté pour les partenaire impaire

--- src/partenaire.vue
+++ src/partenaire.vue
@@ -13,11 +13,12 @@
             <a href="https://www.cnrs.fr/fr" target="Fenêtre définie"><img src="CNRS.png" alt="logo du CNRS" /></a>
         </div>
         <div class="impair">
-            <div class="description">
-                <h4> l'ISJPS</h4>
-                <p>L’Institut des sciences juridique et philosophique de la Sorbonne (ISJPS) réunit des chercheurs, des enseignants-chercheurs, des chercheurs associés, des post-doctorants et des doctorants qui abordent ensemble, à partir de leur expertise disciplinaire, des thématiques stratégiques au cœur de notre société.</p>
-            </div>
-            <a href="https://isjps.pantheonsorbonne.fr/" target="Fenêtre définie"><img src="ISJPS.png" alt="logo du ISJPS" /></a>
+            
+          <a href="https://isjps.pantheonsorbonne.fr/" target="Fenêtre définie"><img src="ISJPS.png" alt="logo du ISJPS" /></a>
+          <div class="description">
+              <h4> l'ISJPS</h4>
+              <p>L’Institut des sciences juridique et philosophique de la Sorbonne (ISJPS) réunit des chercheurs, des enseignants-chercheurs, des chercheurs associés, des post-doctorants et des doctorants qui abordent ensemble, à partir de leur expertise disciplinaire, des thématiques stratégiques au cœur de notre société.</p>
+          </div>
         </div>
         <div class="pair">
             <div class="description">
@@ -27,11 +28,11 @@
             <a href="https://jfli.cnrs.fr/" target="Fenêtre définie"><img src="logo-jfli.png" alt="logo du JFLI" /></a>
         </div>
         <div class="impair">
-            <div class="description">
+          <a href="https://www.betaseries.com/" target="Fenêtre définie"><img src="betaSeries.jfif" alt="logo de BétaSérie" /></a>
+          <div class="description">
             <h4> BétaSérie</h4>
             <p>Une plateforme de recommandation de séries.</p>
-            </div>
-            <a href="https://www.betaseries.com/" target="Fenêtre définie"><img src="betaSeries.jfif" alt="logo de BétaSérie" /></a>
+          </div>
         </div>
     </div>
 </div>
